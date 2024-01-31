8ab25bdb7969ace2ea6163e5311c2789bd6c20cd
rick-sys <ricksanchezy2x@gmail.com>
2024-01-31 20:03:50 +0100

    bugfix

--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -1,23 +1,22 @@
-import React, { useEffect } from 'react'
- 
-const Chatbot = () => {
+import React, { useEffect } from 'react';
+
+// Use this directive to mark the component for client-side only
+export default function Chatbot() {
   useEffect(() => {
-    const script = document.createElement('script')
-    script.src = 'https://cdn.botpress.cloud/webchat/v1/inject.js'
-    script.async = true
-    document.body.appendChild(script)
- 
+    const script = document.createElement('script');
+    script.src = 'https://cdn.botpress.cloud/webchat/v1/inject.js';
+    script.async = true;
+    document.body.appendChild(script);
+
     script.onload = () => {
       window.botpressWebChat.init({
         botId: 'f9805774-fb6c-439e-8d19-dec923fa753e',
         hostUrl: 'https://cdn.botpress.cloud/webchat/v1',
         messagingUrl: 'https://messaging.botpress.cloud',
         clientId: 'f9805774-fb6c-439e-8d19-dec923fa753e',
-      })
-    }
-  }, [])
- 
-  return <div id="webchat" />
+      });
+    };
+  }, []);
+
+  return <div id="webchat" />;
 }
- 
-export default Chatbot
